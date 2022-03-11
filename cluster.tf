9bd42a083b9f10ba80a38cb5ca00e8c967f0063c
Rhys <rhys.davies901@gmail.com>
2022-03-11 09:18:14 -0800

    Adding new capacity_providers resource

--- cluster.tf
+++ cluster.tf
@@ -5,16 +5,19 @@ locals {
 resource "aws_ecs_cluster" "cluster" {
   name = local.cluster_full_name
 
-  capacity_providers = var.include_asg_capacity_provider == "yes" ? [aws_ecs_capacity_provider.autoscaling_group[0].name] : []
-
-  tags = local.tags
-
   setting {
     name  = "containerInsights"
     value = var.enable_container_insights == "yes" ? "enabled" : "disabled"
   }
 
+  tags = local.tags
+
   depends_on = [
     null_resource.iam_wait
   ]
+}
+
+resource "aws_ecs_cluster_capacity_providers" "capacity_providers" {
+  cluster_name       = local.cluster_full_name
+  capacity_providers = var.include_asg_capacity_provider == "yes" ? [aws_ecs_capacity_provider.autoscaling_group[0].name] : []
 }
