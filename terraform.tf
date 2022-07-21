2114aa478199a2af2e0fa122f5f4ea34e605ed15
rhys <rhys.davies901@gmail.com>
2022-07-21 11:33:45 -0700

    Relaxing version constraints

--- terraform.tf
+++ terraform.tf
@@ -1,18 +1,15 @@
 terraform {
-  required_version = ">= 0.14"
+  required_version = ">= 1.0"
 
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = "~> 3.29"
+      source = "hashicorp/aws"
     }
     null = {
       source = "hashicorp/null"
-      version = "~> 3.0"
     }
     template = {
       source = "hashicorp/template"
-      version = "~> 2.2"
     }
   }
 }
